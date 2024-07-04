6d2a2a9c44d0bd859c0b23115971c2bde623a564
JoshuaPruitt <JoshuaPruitt6484@gmail.com>
2024-07-03 19:28:06 -0600

    added some css and html elements to questions.hmtl

---
+++ assets/js/questionsScript.js
@@ -0,0 +1,9 @@
+// code must interface with buttons. 
+// JavaScript will read from storage and append items to page. 
+// First itll show a video. Once the video is finished a player can press the start button. The video will disapear, a timer will start, and questions will append to the page.
+// if the timer finishes before the questions have been answered then a fail will display onto the screen. Then it will go to the next question
+
+let textObj = [{}];
+
+//data read and write to storage
+
