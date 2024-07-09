b64085940f8dfdfd114ca0ca9da05086a644fdf9
Jacob Menlove <jacobmenlove1@gmail.com>
2024-07-08 21:23:36 -0600

    jacobs quiz info

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,17 +9,20 @@ const redirectPage = function(url){
 
 let Obj = {
     //add your video tags here when you create your seperate category. The code switches out the video for the video that q represents. If q = 0 then q will pull up the first video, so on and so forth.
-    Video: ['<iframe width="560" height="315" src="https://www.youtube.com/embed/7AvXEmQU69Q?si=MnfVvYsgfmm98UFA&amp;controls=0" title="YouTube video player" frameborder="0" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>', 
-    
+    Video: ['<iframe width="560" height="315" src="https://www.youtube.com/embed/Ifh-ZndgQyQ?si=Wib6tGs26YT20Gtt" title="YouTube video player" frameborder="0" </iframe>',
+        '<iframe width="560" height="315" src="https://www.youtube.com/embed/vF4iWIE77Ts?si=77mkTwsK7mYHpEiN" title="YouTube video player" frameborder="0" </iframe>',
+        '<iframe width="560" height="315" src="https://www.youtube.com/embed/K-earI3cj5k?si=oqbnE64-VesflShd" title="YouTube video player" frameborder="0" </iframe>',
+        '<iframe width="560" height="315" src="https://www.youtube.com/embed/r1zxtCGDZhs?si=dd88bdZ5MrofzwJA" title="YouTube video player" frameborder="0" </iframe>',
+        '<iframe width="560" height="315" src="https://www.youtube.com/embed/-ONdssiN6qE?si=g35kA9N1pF9y0ZEg" title="YouTube video player" frameborder="0"</iframe>',
     ],
     //this stores all the questions for a category
     questions: [
         //question text stores the question label. Question answer shows the correct answer. q1-q4 are the selection labels
-        {questiontext: 'This is question 1', questionAnswer: 1, q1: 'Hai!', q2: "hellooo!", q3: 'greetings', q4: 'yipppee'},
-        {questiontext: 'This is question 2', questionAnswer: 2, q1: 'a', q2: "b", q3: 'c', q4: 'd'}, 
-        {questiontext: 'This is question 3', questionAnswer: 3, q1: 'b', q2: "c", q3: 'd', q4: 'a'}, 
-        {questiontext: 'This is question 4', questionAnswer: 4, q1: 'c!', q2: "d", q3: 'a', q4: 'b'},
-        {questiontext: 'This is question 5', questionAnswer: 1, q1: 'd!', q2: "a", q3: 'b', q4: 'c'},
+        {questiontext: 'What Colors Were Jordans Shoes?', questionAnswer: 1, q1: 'White', q2: "Red", q3: 'Black', q4: 'Grey'},
+        {questiontext: 'What Color Shirt Was The Referee Wearing?', questionAnswer: 2, q1: 'Blue', q2: "Red", q3: 'Yellow', q4: 'Black'}, 
+        {questiontext: 'What Number Was The Goalie Wearing?', questionAnswer: 3, q1: '28', q2: "97", q3: '29', q4: '9'}, 
+        {questiontext: 'What Was The Score of The Game?', questionAnswer: 4, q1: '0-2', q2: "0-0", q3: '1-1', q4: '1-0'},
+        {questiontext: 'What Area of The Goal Did The Ball Score On?', questionAnswer: 1, q1: 'Top Left', q2: "Bottom Right", q3: 'Bottom left', q4: 'Top Right'},
     ]  
 };
 
