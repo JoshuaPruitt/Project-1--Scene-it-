4c056cb39829e3469b5ccb9209dc7f69f9d4a886
parkerspeares <parker.speares@outlook.com>
2024-07-09 19:51:11 -0700

    Update highscorePage.js

--- assets/js/highscorePage.js
+++ assets/js/highscorePage.js
@@ -13,7 +13,15 @@ function init(){
     totalQuestions = right + wrong
     //get the selection choice
     selectionChoice = finalScores.testNo
+<<<<<<< Updated upstream
 
+=======
+    
+    var correctTotal = .7 * totalQuestions
+    if (right >= correctTotal)
+
+    //returns those variables and rewrites them to have the newly grabbed information
+>>>>>>> Stashed changes
     return
 };
 
