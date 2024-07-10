903bf28789695b9923179dbf0ba8319ddc0b93d5
parkerspeares <parker.speares@outlook.com>
2024-07-09 21:12:06 -0700

    Update highscorePage.js

--- assets/js/highscorePage.js
+++ assets/js/highscorePage.js
@@ -1,8 +1,11 @@
 let right = 0;
 let wrong = 0;
+//total questions is right and wrong added together. 
 let totalQuestions = 0;
+//selection choice is the quiz that was selected. (if someone selected the 'games' quiz this variable will say 'games')
 let selectionChoice;
 
+//init runs on startup. It grabs the variables from storage and 
 function init(){
     //grab storage and set it
     let finalScores = JSON.parse(localStorage.getItem('finalScores'))
@@ -13,19 +16,18 @@ function init(){
     totalQuestions = right + wrong
     //get the selection choice
     selectionChoice = finalScores.testNo
-<<<<<<< Updated upstream
 
-=======
-    
-    var correctTotal = .7 * totalQuestions
-    if (right >= correctTotal)
 
     //returns those variables and rewrites them to have the newly grabbed information
->>>>>>> Stashed changes
     return
 };
 
+//this runs the init function at startup. S
 init()
+
+var correctTotal = .7 * totalQuestions
+if (right >= correctTotal) 
+// you can look at the console to see the variables in action
 console.log('Correct: ' + right)
 console.log('Incorrect: ' + wrong)
 console.log('Total Questions: ' + totalQuestions)
